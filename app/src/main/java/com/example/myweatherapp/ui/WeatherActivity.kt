3273505fca8841582467e3fd0dec3e4639c47c55
blucky8649 <blucky4511@gmail.com>
2022-05-02 16:05:29 +0900

    FEAT: 리사이클러뷰 셋업

--- app/src/main/java/com/example/myweatherapp/ui/WeatherActivity.kt
+++ app/src/main/java/com/example/myweatherapp/ui/WeatherActivity.kt
@@ -5,25 +5,41 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
-import com.example.myweatherapp.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myweatherapp.adapter.WeatherListAdapter
 import com.example.myweatherapp.databinding.ActivityMainBinding
+import com.example.myweatherapp.util.Resource
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class WeatherActivity : AppCompatActivity() {
+    val weatherAdapter: WeatherListAdapter by lazy {
+        WeatherListAdapter()
+    }
     private val viewModel: WeatherViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupRecyclerView()
 
         lifecycleScope.launch {
-            viewModel.state.collectLatest {
-                Log.d("Lee", it.toString())
+            viewModel.state.collectLatest { result ->
+                when(result) {
+                    is Resource.Success -> {
+                        weatherAdapter.submitList(result.data?.toList())
+                    }
+                }
             }
+        }
+    }
+    fun setupRecyclerView() {
+        binding.rvWeatherInfos.apply {
+            adapter = weatherAdapter
+            layoutManager = LinearLayoutManager(this@WeatherActivity)
         }
     }
 }
--- app/src/main/java/com/example/myweatherapp/util/DataParser.kt
+++ app/src/main/java/com/example/myweatherapp/util/DataParser.kt
@@ -5,7 +5,17 @@ import com.example.myweatherapp.model.entity.WeatherEntity
 import java.text.SimpleDateFormat
 import java.util.*
 
-fun Date.toStringFormat(): String {
-    val simpleDateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
-    return simpleDateFormat.format(this)
+
+fun getToday() : String {
+    val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    val curDate = Date()
+    return format.format(curDate)
+}
+
+fun getTomorrow() : String {
+    val cal = Calendar.getInstance()
+    val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    cal.time = Date()
+    cal.add(Calendar.DATE, 1)
+    return format.format(cal.time)
 }
