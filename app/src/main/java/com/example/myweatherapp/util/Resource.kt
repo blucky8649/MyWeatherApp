52236e9a5cbf4309f3cc78dd559ea24cc4717562
blucky8649 <blucky4511@gmail.com>
2022-05-04 21:59:22 +0900

    FEAT: 최종 점검

--- app/src/main/java/com/example/myweatherapp/util/Resource.kt
+++ app/src/main/java/com/example/myweatherapp/util/Resource.kt
@@ -1,7 +1,5 @@
 package com.example.myweatherapp.util
 
-// retrofit2을 통한 통신이 성공인지 실패인지 나타내기 위한 클래스
-// sealed class 는 동일 파일에 정의된 하위 클래스 외에 다른 하위 클래스는 존재하지 않는다는 것을 의미한다.
 sealed class Resource<T> (
     val data: T? = null,
     val message: String? = null
