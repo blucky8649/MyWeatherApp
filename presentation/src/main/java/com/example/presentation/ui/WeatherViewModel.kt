5395f190f1c309f446903bcf3df4a82fa839f686
blucky8649 <blucky4511@gmail.com>
2022-09-26 01:49:59 +0900

    FEAT: uiState 적용중

--- presentation/src/main/java/com/example/presentation/ui/WeatherViewModel.kt
+++ presentation/src/main/java/com/example/presentation/ui/WeatherViewModel.kt
@@ -7,9 +7,7 @@ import com.example.domain.entity.Weather
 import com.example.domain.usecases.GetWeatherListUseCase
 import com.example.domain.usecases.WeatherUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import java.io.IOException
 import javax.inject.Inject
@@ -20,15 +18,43 @@ class WeatherViewModel @Inject constructor(
     private val getWeatherListUseCase: GetWeatherListUseCase
 ) : ViewModel() {
 
-    private val _weatherState = MutableStateFlow(WeatherUiState())
-    val weatherState get() = _weatherState as StateFlow<WeatherUiState>
-
     val _state: MutableStateFlow<Resource<List<Weather>>> = MutableStateFlow(Resource.Loading())
     val state get() = _state
 
     init {
         observeWeather(fetchFromRemote = false)
     }
+    suspend fun getWeatherFlow(
+        query: String = "se",
+        fetchFromRemote: Boolean
+    ): StateFlow<WeatherUiState> {
+        return getWeatherListUseCase(query, fetchFromRemote)
+            .onStart { emit(Resource.Loading()) }
+            .map { weather -> produceWeatherUiState(weather) }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(StopTimeoutMillis),
+                initialValue = WeatherUiState(isLoading = true)
+            )
+    }
+    private val StopTimeoutMillis: Long = 5000
+
+    fun produceWeatherUiState(taskLoad: Resource<List<Weather>>): WeatherUiState =
+        when (taskLoad) {
+            is Resource.Loading -> {
+                WeatherUiState(isLoading = true)
+            }
+            is Resource.Success -> {
+                taskLoad.data?.let {
+                    WeatherUiState(
+                        isLoading = false,
+                        isRefreshing = false,
+                        WeatherList = it
+                    )
+                } ?: WeatherUiState(isLoading = false, errorMassages = listOf("Error Occured"))
+            }
+            else -> { WeatherUiState(isLoading = false, errorMassages = listOf("Error Occured")) }
+        }
 
     fun observeWeather(
         query: String = "se",
